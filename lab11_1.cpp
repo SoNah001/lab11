0474ce002101c259d3fe21a4653297a5bf7d4f1e
Supanat <k.suphanuch23@gmail.com>
2024-12-20 16:51:21 +0700

    lap11_1 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int grade=rand()%9+1;
+    if(grade==1) cout << "You will get A in this 261102.";
+    else if(grade==2) cout << "You will get B+ in this 261102.";
+    else if(grade==3) cout << "You will get B in this 261102.";
+    else if(grade==4) cout << "You will get C+ in this 261102.";
+    else if(grade==5) cout << "You will get C in this 261102.";
+    else if(grade==6) cout << "You will get D+ in this 261102.";
+    else if(grade==7) cout << "You will get D in this 261102.";
+    else if(grade==8) cout << "You will get F in this 261102.";
+    else cout << "You will get W in this 261102.";
+}
+
+
 // cout << "You will get A in this 261102.";
